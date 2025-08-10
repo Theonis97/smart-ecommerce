9615d3e87e4815fda47eb8983cde3342480d2466
Theonis97 <boussougoumvoumbitheonisjozhy@gmail.com>
2025-08-10 14:14:11 +0100

    second commit

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+import Navbar from "./components/Navbar";
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Page() {
+
+    return (
+        <div>
+            <h1>hello theo</h1>
+        </div>
+    )
+}
